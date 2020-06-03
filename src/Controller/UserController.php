2b087f82dea7f4670eea87e4f089a1253f0d0248
andyl <andy.lesec@gmail.com>
2020-06-03 15:23:58 +0200

    profile: reste controller a terminer

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,7 +10,7 @@ class UserController extends AbstractController
     /**
      * @Route("/user", name="user")
      */
-    public function index()
+    public function index(SessionInterface $session)
     {
         return $this->render('user/index.html.twig', [
             'controller_name' => 'UserController',

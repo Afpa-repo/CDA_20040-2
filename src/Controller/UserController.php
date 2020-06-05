df69de0e3dc0409f32f816ee87074b3126f230b0
andyl <andy.lesec@gmail.com>
2020-06-05 09:39:50 +0200

    profile update

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserController extends AbstractController
 {
@@ -11,10 +15,10 @@ class UserController extends AbstractController
      * @Route("/user", name="user")
      */
     
-    public function index()
+    public function index(UserRepository $userRepository)
     {
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            'user' => $userRepository->findAll(),
         ]);
     }
 }
